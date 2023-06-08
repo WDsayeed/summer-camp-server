707a3e1642df8816be36f7b8d2a2bcd01eb3eeeb
WDsayeed <sayeedahammed526@gmail.comm>
2023-06-08 18:31:36 +0600

    added server connection

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvg3x0p.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
