0585fff1c600351cb5aa6126c94dde04bedca862
WDsayeed <sayeedahammed526@gmail.comm>
2023-06-14 06:38:54 +0600

    add mongodb

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-// globalLanguage
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
